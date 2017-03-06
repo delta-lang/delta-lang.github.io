787bdcb08fa8ad7d92393a3368bee58942d50591
emlai <emil@cs.helsinki.fi>
2017-03-06 23:54:50 +0200

    Use BOOST_ROOT instead of CMAKE_PREFIX_PATH for Boost

--- install_delta.sh
+++ install_delta.sh
@@ -16,5 +16,5 @@ if [ ! -d clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 ]; then
     curl -L "http://releases.llvm.org/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz" | tar xJ
 fi
 
-cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH='$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04;boost_1_63_0' .
+cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 -DBOOST_ROOT=$PWD/boost_1_63_0 .
 make

05118443032863cebe650bf58f5077072737dc23
emlai <emil@cs.helsinki.fi>
2017-03-07 00:02:05 +0200

    Remove downloaded stuff after build to reduce slug size
    Except Clang because we need some of Clang's headers when compiling
    Delta source code.

--- install_delta.sh
+++ install_delta.sh
@@ -4,17 +4,13 @@ fi
 
 cd delta
 
-if [ ! -d cmake-3.1.3-Linux-x86_64 ]; then
-    curl -L "https://cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz" | tar xz
-fi
-
-if [ ! -d boost_1_63_0 ]; then
-    curl -L "https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fboost%2Ffiles%2Fboost%2F1.63.0%2F&ts=1488836101&use_mirror=netcologne" | tar xz
-fi
-
-if [ ! -d clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 ]; then
-    curl -L "http://releases.llvm.org/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz" | tar xJ
-fi
+curl -L "https://cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz" | tar xz
+curl -L "https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fboost%2Ffiles%2Fboost%2F1.63.0%2F&ts=1488836101&use_mirror=netcologne" | tar xz
+curl -L "http://releases.llvm.org/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz" | tar xJ
 
 cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 -DBOOST_ROOT=$PWD/boost_1_63_0 .
 make
+
+# Reduce size for Heroku slug compression.
+rm -rf cmake-3.1.3-Linux-x86_64
+rm -rf boost_1_63_0

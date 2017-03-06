df5063f5a5a4d125b99badcc6e77a263be3c6775
emlai <emil@cs.helsinki.fi>
2017-03-06 23:13:23 +0200

    Download prebuilt CMake for Heroku

--- install_delta.sh
+++ install_delta.sh
@@ -4,9 +4,13 @@ fi
 
 cd delta
 
+if [ ! -d cmake-3.1.3-Linux-x86_64 ]; then
+    curl -L https://cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz | tar xz
+fi
+
 if [ ! -d clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 ]; then
     curl -L http://releases.llvm.org/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar xJ
 fi
 
-cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 .
+cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 .
 make

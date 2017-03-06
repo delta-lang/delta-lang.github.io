4242f1771cb817c981cdf1e3bfbc2210037649b9
emlai <emil@cs.helsinki.fi>
2017-03-06 23:16:13 +0200

    Download prebuilt Boost for Heroku

--- install_delta.sh
+++ install_delta.sh
@@ -8,9 +8,13 @@ if [ ! -d cmake-3.1.3-Linux-x86_64 ]; then
     curl -L https://cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz | tar xz
 fi
 
+if [ ! -d boost_1_63_0 ]; then
+    curl -L https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz | tar xz
+fi
+
 if [ ! -d clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 ]; then
     curl -L http://releases.llvm.org/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar xJ
 fi
 
-cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04 .
+cmake-3.1.3-Linux-x86_64/bin/cmake -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH='$PWD/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04;boost_1_63_0' .
 make

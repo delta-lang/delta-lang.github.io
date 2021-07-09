ff485e1ce9e42b024d1eba4af93b71bc7f1b40c6
Emil Laine <laine.emil@gmail.com>
2021-07-09 18:05:27 +0300

    Remove custom cmake installation
    heroku-20 stack includes cmake 3.16 by default

--- server/install-cx.sh
+++ server/install-cx.sh
@@ -12,17 +12,15 @@ git checkout $CX_VERSION
 LLVM_VERSION=$(grep -oP 'LLVM_VERSION: (\K.*)' .github/workflows/build.yml)
 LLVM_TARBALL=clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-20.04
 
-curl -L "https://github.com/Kitware/CMake/releases/download/v3.15.0/cmake-3.15.0-Linux-x86_64.tar.gz" | tar xz
 curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVM_TARBALL.tar.xz" | tar xJ
 
-cmake-3.15.0-Linux-x86_64/bin/cmake . \
+cmake . \
     -G 'Unix Makefiles' \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_PREFIX_PATH=$PWD/$LLVM_TARBALL
 make -j
 
 # Reduce size for Heroku slug compression.
-rm -rf cmake-3.15.0-Linux-x86_64
 mkdir -p ../lib/clang/$LLVM_VERSION
 mv $LLVM_TARBALL/lib/clang/$LLVM_VERSION/include ../lib/clang/$LLVM_VERSION
 rm -rf $LLVM_TARBALL
